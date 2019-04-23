47d0abc8d89908a65c6bf63b8662c15ad4397a35
Ben Nelson <benjamin.n.nelson@gmail.com>
2019-04-22 21:58:06 -0400

    First commit

---
+++ dataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dataStructures
+//
+//  Created by Ben Nelson on 4/22/19.
+//  Copyright © 2019 Ben Nelson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
